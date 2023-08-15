b8eb10f8d14c2e99a3889740e1aa256ab43d654a
saevans86 <sam_evans1986@hotmail.com>
2023-08-15 17:03:30 -0600

    file now writing and creating readme, pending displaying lincense badge and links

--- Develop/index.js
+++ Develop/index.js
@@ -1,48 +1,102 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const ulti = require("util");
-const inq = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const renderLicenseBadge = require("./utils/generateMarkdown")
+// const renderLicenseLink = require("./utils/generateMarkdown")
 const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [
-   {type: "input",
-   name: "title",
-   message: "What is the title of your project?"},
-   {type: "input",
-   name: "description",
-   message: "Descriiption of project?"},
-   {type: "input",
-   name: "installation",
-   message: "What is needed for installation?"},
-   {type: "input",
-   name: "usage",
-   message: "instructions for use?"},
-   {type: "input",
-   name: "credits",
-   message: "Please list credits/collaborators"},
-   {type: "checkbox",
-   name: "License",
-   message: "Select the license for your project",
-   choices: ["gpl-3.0", "MIT", "Apache-2.0"]},
-   {type: "input",
-   name: "Features",
-   message: "Does your project have any features?"},    
+// const questions = [
+//    {type: "input",
+//    name: "title",
+//    message: "What is the title of your project?"},
+//    {type: "input",
+//    name: "description",
+//    message: "Descriiption of project?"},
+//    {type: "input",
+//    name: "installation",
+//    message: "What is needed for installation?"},
+//    {type: "input",
+//    name: "usage",
+//    message: "instructions for use?"},
+//    {type: "input",
+//    name: "credits",
+//    message: "Please list credits/collaborators"},
+//    {type: "checkbox",
+//    name: "License",
+//    message: "Select the license for your project",
+//    choices: ["gpl-3.0", "MIT", "Apache-2.0"]},
+//    {type: "input",
+//    name: "Features",
+//    message: "Does your project have any features?"},    
   
-];
-console.log(questions)
-// const prompt = require("prompt-sync")();
+// ];
+
+
 
 // TODO: Create a function to write README file
- async function writeToFile(fileName, data) {}
+//  async function writeToFile(fileName, data) {
+// }
 
 
-// TODO: Create a function to initialize app
+async function writeToFile() {
+   try {
+     const questions = await inquirer.prompt([
+       {type: "input",
+    name: "title",
+    message: "What is the title of your project?"},
+    {type: "input",
+    name: "description",
+    message: "Provide a short description explaining the what, why, and how of your project. "},
+    {type: "input",
+    name: "installation",
+    message: "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running."},
+    {type: "input",
+    name: "usage",
+    message: "Provide instructions and examples for use. Include screenshots as needed."},
+    {type: "input",
+    name: "credits",
+    message: "List your collaborators, if any, with links to their GitHub profiles."},
+    {type: "checkbox",
+    name: "license",
+    message: "Select the license for your project",
+    choices: ["GPL v3.0", "MIT", " Apache-2.0 ", "None",]},
+    {type: "input",
+    name: "features",
+    message: "Does your project have any features?"}, 
+    {type: "input",
+    name: "tests",
+    message: "what tests have been performed? Please provide examples of how to run them."},    
+        ]);
+ 
+       const userResponse = generateMarkdown(questions);
+      //  const licenseBadge = renderLicenseBadge(questions.license)
+
+          //   let readmeContent = `# ${questions.title}\n${questions.description}\n${questions.installation}
+   //   \n${questions.usage}\n${questions.credits}\n${questions.License}\n${questions.usage}\n${questions.Features}`;
+ 
+     fs.writeFileSync('NEWREADME.md', userResponse);
+     
+    
+     
+ 
+     console.log('You did it Bestie!');
+   } catch (error) {
+     console.error('Error generating NEWREADME:', error);
+     
+   }
+ }
+ 
+//  TODO: Create a function to initialize app
 function init() {
-    writeToFile()
+   writeToFile() 
+   
 }
+    
+
 
 // Function call to initialize app
 init();
-
+ 
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,21 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-//[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+// const js = require("index.js")
+function renderLicenseBadge(license) {
+  // let questions = `${questions.license}`;
+  if (license === "None") {
+    return "";
+  } else if (license === "GPL v3.0") {
+    return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+  } else if (license === "MIT") {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  } else if (license === "Apache-2.0") {
+    return "[![License]: Apache-2.0 (https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+  } else {
+    return "";
+  }
+}
+//[![License]: Apache-2.0 (https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
 //[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 //[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
 
@@ -17,13 +31,66 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+ 
+  return `
+# Title ${data.title}
+
+
+Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+
+## Description ${data.description}
+
+
+
+
+
+## Table of Contents (Optional)
+
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+
+## Installation ${data.installation}
+
+
+
+## Usage ${data.usage}
+
+
+
+    
+
+## Credits ${data.credits}
+
+
+
+## License ${data.license}
+
+
+---
+
+
+
+
+
+
+## Features ${data.features}
+
+
+
+
+
+Go the extra mile and write tests for your application. Then provide examples on how to run them here.
+
+## Tests ${data.tests}
+
+
 
 `;
+
 }
 
 module.exports = generateMarkdown;
-
-
-
-//https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
