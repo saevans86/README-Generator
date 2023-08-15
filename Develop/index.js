b5943af961765fe5dd52029adcca5e541a521c5c
saevans86 <sam_evans1986@hotmail.com>
2023-08-14 18:29:09 -0600

    questions writte, pending func to write file, and if for licenses

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,48 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const ulti = require("util");
+const inq = require("inquirer");
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+   {type: "input",
+   name: "title",
+   message: "What is the title of your project?"},
+   {type: "input",
+   name: "description",
+   message: "Descriiption of project?"},
+   {type: "input",
+   name: "installation",
+   message: "What is needed for installation?"},
+   {type: "input",
+   name: "usage",
+   message: "instructions for use?"},
+   {type: "input",
+   name: "credits",
+   message: "Please list credits/collaborators"},
+   {type: "checkbox",
+   name: "License",
+   message: "Select the license for your project",
+   choices: ["gpl-3.0", "MIT", "Apache-2.0"]},
+   {type: "input",
+   name: "Features",
+   message: "Does your project have any features?"},    
+  
+];
+console.log(questions)
+// const prompt = require("prompt-sync")();
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+ async function writeToFile(fileName, data) {}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    writeToFile()
+}
 
 // Function call to initialize app
 init();
+
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,10 +1,15 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
+//[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+//[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+//[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -18,3 +23,7 @@ function generateMarkdown(data) {
 }
 
 module.exports = generateMarkdown;
+
+
+
+//https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
